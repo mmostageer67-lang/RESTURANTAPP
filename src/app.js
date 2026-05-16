1052c05adc852d3824c28c91cc49270aa2122365
mahmoud mostageer <mmostageer67@gmail.com>
2026-05-16 23:47:12 +0300

    Wire up auth register route

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
 const errorHandler = require('./middlewares/error.middleware');
-
+const router = require('./routes/index');
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
 
+app.use('/api', router);
 
 app.use(errorHandler);
 
---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+const  express  = require('express');
+const authRoutes = require('../modules/auth/auth.routes');
+
+const router = express.Router();
+router.use('/auth', authRoutes);
+
+module.exports = router;
