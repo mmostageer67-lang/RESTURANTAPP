99a3b8bb17bfabe981acec61d5e3552925cd7de8
mahmoud mostageer <mmostageer67@gmail.com>
2026-05-16 22:37:44 +0300

    auth register

---
+++ src/modules/auth/auth.service.js
@@ -0,0 +1,33 @@
+const User = require('../user/user.model');
+
+const register = async ({ name, email, password, role, phone, address }) => {
+  const existingUser = await User.findOne({ email }).lean();
+  if (existingUser) {
+    const error = new Error('Email already in use');
+    error.statusCode = 409;
+    throw error;
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+    phone,
+    address,
+  });
+
+
+  return {
+    user: {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      phone: user.phone,
+      address: user.address,
+    },
+  };
+};
+
+module.exports = { register };
